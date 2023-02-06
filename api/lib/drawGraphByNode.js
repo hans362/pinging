6fc3f5ec24d32f9af84b19c2aa3514d913a0dbf9
hans362 <i@hans362.cn>
2023-02-06 13:27:49 +0800

    :sparkles: Add packet loss view

--- api/lib/drawGraphByNode.js
+++ api/lib/drawGraphByNode.js
@@ -8,11 +8,22 @@ async function processRecords(records) {
   for await (const agentId of agentIds) {
     const agentRecords = records.filter(record => record.agentId === agentId);
     const data = agentRecords.map(record => [Date.parse(record.createdAt), record.latency == -1 ? null : record.latency]);
+    const loss = agentRecords.filter(record => record.loss != 0 || record.latency == -1).map(record => {
+      const next = agentRecords.find(nextRecord => nextRecord.createdAt > record.createdAt);
+      return [{ xAxis: Date.parse(record.createdAt) }, { xAxis: Date.parse(next ? next.createdAt : record.createdAt) }];
+    });
     series.push({
       name: await getAgentNameById(agentId),
       type: 'line',
       data: data,
       smooth: true,
+      markArea: {
+        silent: true,
+        data: loss,
+        itemStyle: {
+          opacity: 0.25,
+        },
+      },
     });
   }
   return series;
